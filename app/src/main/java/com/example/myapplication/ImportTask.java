14e1a6a8cbe21402295ab4afc1e9f41f9ed4686a
Walker Menkus <wmenkus1@gmail.com>
2022-11-09 15:00:00 -0500

    created Add Task page and empty New Task and Import Task pages

---
+++ app/src/main/java/com/example/myapplication/ImportTask.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImportTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_import_task);
+    }
+}

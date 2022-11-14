082eef5bb5515da39e8f9001c3a903bf8e915cc0
ocaldwell01 <odarriuscaldwell01@gmail.com>
2022-11-14 01:08:18 -0500

    layout changes

--- app/src/main/java/com/example/myapplication/ChatPage.java
+++ app/src/main/java/com/example/myapplication/ChatPage.java
@@ -1,14 +1,30 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
 
-public class FriendActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class ChatPage extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private ArrayList<Chat> chats;
+    private String username = "example1";
+    private String username2 = "example2";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_friend);
+        setContentView(R.layout.chat_page);
+        recyclerView = findViewById(R.id.recyclerview);
+        chats = new ArrayList<>();
+        }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_menu, menu);
+        return true;
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
-public class MainActivity extends AppCompatActivity {
+/*public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+}*/
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chat_page);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_menu, menu);
+        return true;
     }
 }

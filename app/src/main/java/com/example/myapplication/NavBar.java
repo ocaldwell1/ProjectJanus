45e1d31ca66f5d9da41db2eb1fc0d2885d88b0db
Walker Menkus <wmenkus1@gmail.com>
2022-11-14 22:50:04 -0500

    initial navigation setup for #43

---
+++ app/src/main/java/com/example/myapplication/NavBar.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+
+import android.os.Bundle;
+
+public class NavBar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nav_bar);
+    }
+}

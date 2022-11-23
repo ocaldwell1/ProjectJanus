1e7b7ff4bc2cb5a062143a8083641a300f8cf017
Walker Menkus <wmenkus1@gmail.com>
2022-11-22 20:49:30 -0500

    add start screen and automatic navigation to login sequence

--- app/src/main/java/com/example/myapplication/StartFragment.java
+++ app/src/main/java/com/example/myapplication/StartFragment.java
@@ -3,24 +3,19 @@ package com.example.myapplication;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import org.checkerframework.checker.nullness.qual.NonNull;
-import org.checkerframework.checker.nullness.qual.Nullable;
-
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link UpcomingTasksFragment#newInstance} factory method to
+ * Use the {@link StartFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class UpcomingTasksFragment extends Fragment {
+public class StartFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,9 +25,8 @@ public class UpcomingTasksFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-    private Button addTaskButton;
 
-    public UpcomingTasksFragment() {
+    public StartFragment() {
         // Required empty public constructor
     }
 
@@ -42,11 +36,11 @@ public class UpcomingTasksFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment TaskFragment.
+     * @return A new instance of fragment StartFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static UpcomingTasksFragment newInstance(String param1, String param2) {
-        UpcomingTasksFragment fragment = new UpcomingTasksFragment();
+    public static StartFragment newInstance(String param1, String param2) {
+        StartFragment fragment = new StartFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -67,28 +61,14 @@ public class UpcomingTasksFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_task, container, false);
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        Button addTaskButton = (Button) view.findViewById(R.id.taskFragmentAddTaskButton);
-        final NavController navController = Navigation.findNavController(view);
-        addTaskButton.setOnClickListener(new View.OnClickListener() {
+        View view = inflater.inflate(R.layout.fragment_start, container, false);
+        Button startFragmentStartButton = (Button) view.findViewById(R.id.startFragmentStartButton);
+        startFragmentStartButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                navController.navigate(R.id.action_taskFragment_to_addTaskFragment);
+                Navigation.findNavController(view).navigate(R.id.action_startFragment_to_taskFragment);
             }
         });
-        MainActivity activity = (MainActivity) requireActivity();
-        User user = activity.user;
-        Log.i("TASK", "user: " + user.isLoggedIn());
-        if(user.isLoggedIn()){
-            Log.i("TASK FRAG", activity.user.getFirstName());
-            navController.navigate(R.id.action_taskFragment_to_menuFragment);
-        }
+        return view;
     }
-
-
-
 }

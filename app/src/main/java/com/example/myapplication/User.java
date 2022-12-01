29d6f458d6aae7b98b49ff30848429df961f274b
Jackie Schellberg <69927792+repojackie@users.noreply.github.com>
2022-11-30 20:01:06 -0500

    Testing position

--- app/src/main/java/com/example/myapplication/User.java
+++ app/src/main/java/com/example/myapplication/User.java
@@ -44,7 +44,7 @@ public class User {
         fUser = mAuth.getCurrentUser();
 
         taskList = new ArrayList<Task>();
-
+        this.taskPosition = 0;
 
         if (fUser != null){
             String userID = fUser.getUid();

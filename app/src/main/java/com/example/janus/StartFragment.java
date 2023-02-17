c994d65a8128eca20c1aed7e7f50e467a02bc694
Jackie Schellberg <69927792+repojackie@users.noreply.github.com>
2023-02-16 19:54:36 -0500

    #123 Basic fix

--- app/src/main/java/com/example/janus/StartFragment.java
+++ app/src/main/java/com/example/janus/StartFragment.java
@@ -1,20 +1,13 @@
 package com.example.janus;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link StartFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class StartFragment extends Fragment {
 
 
@@ -35,6 +28,8 @@ public class StartFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        TaskList.getInstance().getTaskList();
+
         // Inflate the layout for this fragment
         getActivity().setTitle("Start");
         View view = inflater.inflate(R.layout.fragment_start, container, false);
